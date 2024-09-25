cf4df0d96b1a02c785d165ae3bc4978fc90f146c
KongeGeden <hsfacius@gmail.com>
2024-09-25 12:09:22 +0200

    Statustjek

---
+++ Sketchfile.py
@@ -0,0 +1,6 @@
+import sympy as sp
+
+text="a,b,x,s,d,d"
+
+liste=text.split(",")
+print(liste[1])
--- pythobra_backend.py
+++ pythobra_backend.py
@@ -1,13 +1,34 @@
 import sympy as sp
 import matplotlib.pyplot as plt
-
+from sympy.parsing.sympy_parser import parse_expr
 
 class PythobraBackend:
     def __init__(self):
         pass
 
     def solve_equation(self, equation, unknown):
-        return "I må selv finde ud af at få koden til at gøre det rigtige, men jeg skal selvfølgelig nok hjælpe jer. :)"
+        #unknown=sp.Symbol(unknown)
+        hvor_mange=equation.count(",")+1
+        liste_af_ligninger=[]
+        liste_med_uligninge=equation.split(",")
+        liste_af_cha=unknown.split(",")
+        liste_med_symb=[]
+        for cha in liste_af_cha:
+            liste_med_symb.append(sp.Symbol(cha))
+
+        #print(unknown)
+        for i in range(hvor_mange):
+            funktionsstreng, ligemed = liste_med_uligninge[i].split("=")
+            #liste_med_første_side.append
+            ligning=(parse_expr(funktionsstreng))
+            anden_side=(parse_expr(ligemed))
+            #iste_med_anden_side.append
+            liste_af_ligninger.append(sp.Eq(ligning,anden_side))
+
+
+        losning = (sp.solve(liste_af_ligninger,liste_med_symb,dict=True))
+        return str(losning)
+        #return "I må selv finde ud af at få koden til at gøre det rigtige, men jeg skal selvfølgelig nok hjælpe jer. :)"
 
     def plot_function(
         self,
